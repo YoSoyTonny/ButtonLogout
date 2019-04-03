cec068bee90021a811b27c5fd27f930c41566b32
tonny <itonygame@gmail.com>
2019-04-02 19:08:33 -0700

    Initial commit

---
+++ app/src/main/java/com/example/botonlogout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.botonlogout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
